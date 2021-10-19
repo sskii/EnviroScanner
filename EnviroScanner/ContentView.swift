f016945846f91c3312db67666d3f2a35b9e66227
Sam Skinner <30613658+sskii@users.noreply.github.com>
2021-10-20 11:19:06 +1300

    Added standard views & mocked up initial scene

--- EnviroScanner/ContentView.swift
+++ EnviroScanner/ContentView.swift
@@ -8,10 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var currentScene: Int = 1;
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            NavBar()
+            
+            Spacer()
+            
+            if (currentScene == 1) {
+                ScannerScene()
+            }
+            
+            Spacer()
+            
+        }
+        
     }
+}
+
+struct NavBar: View {
+    // Pradyun to supply
+    
+    var body: some View {
+        
+        HStack {
+            
+            Text("Navbar Goes Here")
+                .foregroundColor(.black)
+            
+        }
+        .frame(maxWidth: .infinity)
+        
+        
+    }
+    
+}
+
+struct ScannerScene: View {
+    
+	@State private var itemAreaExpanded = false
+    
+	var body: some View {
+		
+		GeometryReader { geo in
+			ZStack {
+				
+				VStack {
+					Text("Competitive progress bar goes here")
+					Text("Scan preview goes here and fills up remaining space")
+					Text("View size:  (\(geo.size.width), \(geo.size.height))")
+					
+					Image("scanPreview")
+						.resizable()
+						.aspectRatio(contentMode: .fill)
+						.frame(maxWidth: geo.size.width)
+					Spacer()
+				}
+				
+				BottomSheetView(
+					isOpen: self.$itemAreaExpanded,
+					maxHeight: geo.size.height
+				) {
+					Text("This is where we will provide tips at the app's first launch. Then when our user goes ahead and starts scanning, we'll switch over to showing a preview of the last scan and a specific tip to follow. User can swipe up for more info at any point.")
+						.padding()
+				}
+				
+			}.frame(maxWidth: .infinity)
+			
+		}
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- EnviroScanner/StandardViews.swift
+++ EnviroScanner/StandardViews.swift
@@ -19,6 +19,32 @@ struct StandardViews: View {
     }
 }
 
+// standard title element
+struct Title: View {
+    
+    var text: String;
+    
+    var body: some View {
+        Text(text)
+            .font(.system(size: 28, weight: .medium))
+            .foregroundColor(Color("mediumGreen"))
+    }
+    
+}
+
+// port in VisualEffectView
+// https://stackoverflow.com/questions/56610957/is-there-a-method-to-blur-a-background-in-swiftui
+/*
+ Useage:
+	VisualEffectView(effect: UIBlurEffect(style: .dark))
+ */
+struct VisualEffectView: UIViewRepresentable {
+	var effect: UIVisualEffect?
+	func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
+	func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
+}
+
+
 struct StandardViews_Previews: PreviewProvider {
     static var previews: some View {
         StandardViews()
