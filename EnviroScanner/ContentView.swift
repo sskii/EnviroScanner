88d00a8f232a20174030de974709289e550d09ca
Sam Skinner <30613658+sskii@users.noreply.github.com>
2021-10-20 09:17:18 +1300

    Initial Commit

---
+++ EnviroScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnviroScanner
+//
+//  Created by Sam on 20/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EnviroScanner/EnviroScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnviroScannerApp.swift
+//  EnviroScanner
+//
+//  Created by Sam on 20/10/21.
+//
+
+import SwiftUI
+
+@main
+struct EnviroScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
